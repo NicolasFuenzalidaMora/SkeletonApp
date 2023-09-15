e43a367412d0c594561a3b99549110262108a629
NicolasFuenzalidaMora <145055493+NicolasFuenzalidaMora@users.noreply.github.com>
2023-09-15 19:58:03 -0300

    se agrega el proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,88 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { DateRange } from '@angular/material/datepicker';
+import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { trigger, state, style, animate, transition } from '@angular/animations'; // Importa las animaciones
+
+import { FormGroup, FormControl } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  animations: [ // Agrega las animaciones aquí
+    trigger('vibrate', [
+      state('vibrating', style({ transform: 'translateX(5px)' })),
+      state('not-vibrating', style({ transform: 'translateX(0)' })),
+      transition('not-vibrating => vibrating', animate('10s ease-in')),
+      transition('vibrating => not-vibrating', animate('10s ease-out'))
+    ])
+  ]
+})
+export class HomePage {
+  usuario: any = {
+    nivelEducacional: null,
+  };
+
+  inputVibrationState: 'vibrating' | 'not-vibrating' = 'not-vibrating';
+
+  vibrateInputs() {
+    this.inputVibrationState = 'vibrating';
+    setTimeout(() => {
+      this.inputVibrationState = 'not-vibrating';
+    }, 200);
+  }
+
+  mostrarInfoUsuario: boolean = false;
+
+  constructor(public alertController: AlertController) {}
+
+  async mostrarInformacion() {
+    if (!this.usuario.nombre || !this.usuario.apellido) {
+      const alert = await this.alertController.create({
+        header: 'Información incompleta',
+        message: 'Por favor, ingrese tanto el nombre como el apellido.',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+      return;
+    }
+
+    const nivelEducacional = this.usuario.nivelEducacional || 'No especificado';
+
+    const alert = await this.alertController.create({
+      header: 'Información del Usuario',
+      message: `Su nombre de usuario es ${this.usuario.nombre} ${this.usuario.apellido} .`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  limpiarCampos() {
+    this.usuario = {
+      nivelEducacional: null,
+    };
+    this.cambiarColorToolbar();
+    this.vibrateInputs(); // Activa la animación de vibración
+  }
+
+  cambiarColorToolbar() {
+    const toolbar = document.querySelector('ion-toolbar');
+    if (toolbar) {
+      toolbar.classList.remove('color-change-animation', 'primary-toolbar', 'danger-toolbar');
+      setTimeout(() => {
+        const currentColor = toolbar.classList.contains('primary-toolbar') ? 'primary' : 'danger';
+        toolbar.classList.add('color-change-animation', `${currentColor}-toolbar`);
+      }, 10);
+    }
+  }
+
+  mostrarCalendario() {
+    this.mostrarInfoUsuario = true;
+  }
+}
